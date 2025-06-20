f873fc1534594a40f6d20e0c9b483dc90da2b735
Artyom Lyakhov <alyahov49@gmail.com>
2025-06-20 03:26:47 +0300

    подключено к mockAPI, добавлен индикатор загрузки

--- src/presenter/tasks-board-presenter.js
+++ src/presenter/tasks-board-presenter.js
@@ -3,32 +3,30 @@ import TaskComponent from "../../view/task-component.js";
 import TasksBoardComponent from "../../view/tasks-board-component.js";
 import {render, RenderPosition} from "../framework/render.js";
 import ClearButtonComponent from "../../view/clear-button-component.js";
-import { Status, StatusLabel } from "../const.js";
+import { Status, StatusLabel, UserAction} from "../const.js";
 import TasksPlaceholderComponent from "../../view/task-placeholder-component.js";
+import LoadingViewComponent from "../../view/loading-view-component.js";
 
 export default class TasksBoardPresenter {
     #boardContainer = null;
     #tasksModel = null;
 
     #tasksBoardComponent = new TasksBoardComponent();
-    #boardTasks = [];
 
     constructor({ boardContainer, tasksModel }) {
         this.#boardContainer = boardContainer;
         this.#tasksModel = tasksModel;
 
-        this.#tasksModel.addObserver(this.#handleModelChange.bind(this));
-    }
-
-    init() {
-        this.#boardTasks = [...this.#tasksModel.tasks];
-        render(
-            this.#tasksBoardComponent,
-            this.#boardContainer,
-            RenderPosition.AFTEREND
-        );
+        this.#tasksModel.addObserver(this.#handleModelEvent.bind(this));
         this.#renderBoard();
     }
+
+    async init() {
+        await this.#tasksModel.init();
+        this.#clearBoard();
+        this.#renderBoard();
+    }
+
     #renderTask(task, container) {
         const taskComponent = new TaskComponent({ task });
         render(taskComponent, container);
@@ -58,31 +56,37 @@ export default class TasksBoardPresenter {
             onTaskDrop: this.#handleTaskDrop.bind(this),
         });
         render(tasksListComponent, container);
-        if (filteredTasks.length != 0) {
-            filteredTasks.forEach((task) => {
-                this.#renderTask(
-                    task,
-                    tasksListComponent.element.querySelector(
-                        ".tasks-categories ul"
-                    )
-                );
-            });
-        } else {
+
+		if (!this.#tasksModel.isLoaded) {
+            this.#renderLoadingView(tasksListComponent.element.querySelector(
+				".tasks-categories ul"))
+        } else { 
+			if (filteredTasks.length != 0) {
+                filteredTasks.forEach((task) => {
+                    this.#renderTask(
+                        task,
+                        tasksListComponent.element.querySelector(
+                            ".tasks-categories ul"
+                        )
+                    );
+                });
+        	} else {
             this.#renderPlaceholder(
                 tasksListComponent.element.querySelector(".tasks-categories ul")
             );
-        }
-        if (
-            status === Status.BASKET &&
-            this.tasks.some((task) => task.status == Status.BASKET) != false
-        ) {
-            this.#renderClearButton(tasksListComponent.element);
-        }
+        	}
+			if (
+				status === Status.BASKET &&
+				this.tasks.some((task) => task.status == Status.BASKET) != false
+			) {
+				this.#renderClearButton(tasksListComponent.element);
+			}
+		}
     }
     #renderClearButton(container) {
         render(
             new ClearButtonComponent({
-                onClick: () => this.clearTask(),
+                onClick: () => this.#handleClearBasketClick(),
             }),
             container,
             RenderPosition.BEFOREEND
@@ -93,24 +97,29 @@ export default class TasksBoardPresenter {
         render(new TasksPlaceholderComponent(), container);
     }
 
-    createTask() {
+    #renderLoadingView(container) {
+        const loadingView = new LoadingViewComponent();
+        render(loadingView, container, RenderPosition.BEFOREEND);
+    }
+
+    async createTask() {
         const taskTitle = document.querySelector("#task-input").value.trim();
         if (!taskTitle) {
             return;
         }
-        this.#tasksModel.addTask(taskTitle);
-
-        document.querySelector("#task-input").value = "";
+        try {
+            await this.#tasksModel.addTask(taskTitle);
+            document.querySelector("#task-input").value = "";
+        } catch (err) {
+            console.log("Ошибка при создании задачи", err);
+        }
     }
 
     clearTask() {
         this.#tasksModel.removeTask();
+        this.#tasksModel._notifyObservers();
     }
 
-    #handleModelChange() {
-        this.#clearBoard();
-        this.#renderBoard();
-    }
     #clearBoard() {
         this.#tasksBoardComponent.element.innerHTML = "";
     }
@@ -119,7 +128,35 @@ export default class TasksBoardPresenter {
         return this.#tasksModel.tasks;
     }
 
-    #handleTaskDrop(taskID, newStatus, insertIndex){
-		this.#tasksModel.updateTaskStatus(taskID, newStatus, insertIndex)
-	}
+    async #handleTaskDrop(taskID, newStatus, insertIndex) {
+        try {
+            await this.#tasksModel.updateTaskStatus(
+                taskID,
+                newStatus,
+                insertIndex
+            );
+        } catch (err) {
+            console.log("Ошибка при обновлении статуса задачи", err);
+        }
+    }
+
+    #handleModelEvent(event, payload) {
+        switch (event) {
+            case UserAction.ADD_TASK:
+            case UserAction.UPDATE_TASK:
+            case UserAction.DELETE_TASK:
+                this.#clearBoard();
+                this.#renderBoard();
+                break;
+            default:
+                break;
+        }
+    }
+    async #handleClearBasketClick() {
+        try {
+            await this.#tasksModel.clearBasketTasks();
+        } catch (err) {
+            console.error("Ошибка при очистке корзины", err);
+        }
+    }
 }
